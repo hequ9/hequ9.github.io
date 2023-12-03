14df66d5f42458a4c3ce04af0863768045b432d3
hequ9 <hequ9@outlook.com>
2023-12-03 18:27:36 +0800

    chore: edit

--- components/Terminal/TerminalContent.vue
+++ components/Terminal/TerminalContent.vue
@@ -15,7 +15,7 @@
       pb-10
       border-b="1 solid #222324"
     >
-      im 何曲(Hé Qǔ) from China born in 1997, a front-end developer and specialize in Nodejs, Typescript, occasional back-end development, enjoys music, modern history, video games, football, boxing. loves all types of music except folk, most of all R&B & jazz and my favourite game is World of Warcraft(sad😂)
+      im 何曲(Hé Qǔ) from China born in 1997, a front-end developer and specialize in Nodejs, Typescript, occasional back-end development.
     </p>
     <HomeProfileBar />
   </div>
