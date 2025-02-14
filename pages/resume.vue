ea1d311002939f96a0821562cbe8e1942970658a
hequ9 <hequ9@outlook.com>
2025-02-15 05:53:33 +0800

    chore: resume

---
+++ pages/resume.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+//defineProps<{ msg: string }>()
+</script>
+<template>
+<div id="resume">
+this is "resume" component.
+</div>
+</template>
+<style lang="scss" scoped>
+#resume{
+min-height:100vh
+}
+</style>
